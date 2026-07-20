112ba493251040919fbeec5ff813aae60fae9643
WendySozinho-dev <sozinhowendy@gmail.com>
2026-07-20 21:31:34 +0200

    montando a leitura e escrita do arquivo de inicializacao

--- automacao/organizadorNGC_mobile_version/Main.py
+++ automacao/organizadorNGC_mobile_version/Main.py
@@ -4,9 +4,35 @@ from tools.Musculos import musculos
 from tools.Visao import visao
 from tools.Existencia import existe
 from tools.explorerNGC import explorerNGC
+from tools.Json_io import json_read, json_write, json_scan
 import json
 import os
 
+# primeiro, detectar se existe o arquivo de inicialização
+ngc_init = {}
+try:
+    with open("ngc_init.json","r") as file:
+        ngc_init = json.load(file)
+except FileNotFoundError:
+    input("não existe um arquivo de inicialização\npressione [enter] para abrir o explorador  ")
+    initial_dir = explorerNGC().run()
+    input("agora o diretorio final\n[enter para continuar]   ")
+    final_dir = explorerNGC().run()
 
+"""
+agora, no salvamento dos diretórios, não basta só dizer:
+{
+'inicio':'diretótio_inicial',
+'fim':'diretório_final'
+}
+deve ter uma lista inicio e outra lista fim:
+{
+'inicio':['primeiro','segundo'],
+'fim':['primeiro','segundo']
+}
+nota: o indice 1 do inicio tem como fim o indice 1 do fim 
+assim podemos ter vários diretórios para vasculhar arquivos
+"""
 
-
+    
+    
---
+++ automacao/organizadorNGC_mobile_version/tools/.ipynb_checkpoints/Json_io-checkpoint.py
@@ -0,0 +1,43 @@
+# função para escrever e ler um json no armazenamento interno
+import json
+import os
+# função que escaneia um arquivo json
+def json_scan(path,file):
+    #verificando se a a pasta existe
+    if not os.path.exists(path):
+        print(f"{path} não é um diretório")
+        return None
+    
+    # verificando a terminacao
+    if file[-5::] != ".json":
+        print(f"{file} não é um json!")
+        return None
+    else:
+        return file
+
+# função de leitura
+def json_read(path,file):
+    file_content = {}
+    diretory = os.path.join(path,file)
+    try:
+        with open(diretory, "r") as f:
+            file_content = json.load(f)
+
+    except Exception as e:
+        print(e)
+    else:
+        return file_content
+
+
+# função de escrita
+def json_write(path,file,dictionary):
+
+
+    diretory = os.path.join(path,file)
+
+    if not os.path.exists(path):
+        print(f"{path} não é um diretório")
+        return None
+    else:
+        with open(diretory,'w', encoding = 'utf-8') as f:
+            json.dump(dictionary, f, indent = 4, ensure_ascii = False)
